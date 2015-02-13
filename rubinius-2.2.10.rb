dd7bf81669a38c4e346f76d76b0d188ee975565b
Kyle Hargraves <pd@krh.me>
2015-02-13 16:57:13 -0600

    Add rubinius-2.2.10 formula

---
+++ rubinius-2.2.10.rb
@@ -0,0 +1,36 @@
+require 'formula'
+
+class Rubinius2210 < Formula
+  homepage 'http://rubini.us/'
+  url 'https://downloads-rubini-us.s3.amazonaws.com/homebrew/rubinius-2.2.10.tar.bz2'
+  sha1 '75f7540ee72ed2a7226ab0acf94ff3ffadc0a286'
+
+  depends_on 'libyaml'
+
+  depends_on :arch => :x86_64
+  depends_on MinimumMacOSRequirement => :mountain_lion
+
+  keg_only "Conflicts with MRI (Matz's Ruby Implementation)."
+
+  def install
+    # We must install chruby first because eg bin.install moves files.
+    rbx = "rbx-#{version}"
+    rubies = File.expand_path "~/.rubies"
+    rubies_rbx = "#{rubies}/#{rbx}"
+
+    if Dir.exist? rubies and not Dir.exist? rubies_rbx
+      ohai "Installing for chruby..."
+      mkdir_p rubies_rbx
+      cp_r Dir["#{buildpath}/*"], rubies_rbx
+    end
+
+    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*"]
+    include.install Dir["include/*"]
+    man1.install Dir["man/man1/*"]
+  end
+
+  test do
+    assert_equal 'rbx', `"#{bin}/rbx" -e "puts RUBY_ENGINE"`.chomp
+  end
+end

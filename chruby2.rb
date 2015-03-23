73f3ded4a0b3d653ea493535eb9a0fbafacf7e59
Brian Shirai <brixen@gmail.com>
2015-03-23 13:40:37 -0700

    Added chruby2 recipe.

---
+++ chruby2.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class Chruby2 < Formula
+  homepage 'https://github.com/rubinius/chruby#readme'
+  url 'https://github.com/rubinius/chruby/archive/v0.3.9.1.tar.gz'
+  sha1 '7835909fc5eb52591a3a58a7794a46ac651891f2'
+
+  head 'https://github.com/rubinius/chruby.git'
+
+  def install
+    system 'make', 'install', "PREFIX=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to the ~/.bashrc or ~/.zshrc file:
+
+      source #{opt_share}/chruby/chruby.sh
+
+    By default chruby will search for Rubies installed into /opt/rubies/ or
+    ~/.rubies/. For non-standard installation locations, simply set the RUBIES
+    variable after loading chruby.sh:
+
+      RUBIES+=(
+        /opt/jruby-1.7.0
+        $HOME/src/rubinius
+      )
+
+    If you are migrating from another Ruby manager, set `RUBIES` accordingly:
+
+      RVM:   RUBIES+=(~/.rvm/rubies/*)
+      rbenv: RUBIES+=(~/.rbenv/versions/*)
+      rbfu:  RUBIES+=(~/.rbfu/rubies/*)
+
+    To enable auto-switching of Rubies specified by .ruby-version files,
+    add the following to ~/.bashrc or ~/.zshrc:
+
+      source #{opt_share}/chruby/auto.sh
+    EOS
+  end
+end

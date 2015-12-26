ff4ea5885f3f12f5ac2ac66e9b83b7559b7a2ec4
Brian Shirai <brian@rubinius.com>
2015-12-25 22:08:36 -0800

    Version 2.9

--- rubinius.rb
+++ rubinius.rb
@@ -1,2 +1,25 @@
-require File.expand_path('../rubinius-2.8', __FILE__)
-Rubinius = Rubinius28
+require 'formula'
+
+class Rubinius < Formula
+  homepage 'http://rubinius.com/'
+  url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-2.9.tar.bz2'
+  sha1 '4257ae7091bf0c0b72f2d483f94b0ed3c2f293c7'
+
+  depends_on 'libyaml'
+
+  depends_on :arch => :x86_64
+  depends_on MinimumMacOSRequirement => :mountain_lion
+
+  keg_only "Conflicts with MRI (Matz's Ruby Implementation)."
+
+  def install
+    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*"]
+    include.install Dir["include/*"]
+    man1.install Dir["man/man1/*"]
+  end
+
+  test do
+    assert_equal 'rbx', `"#{bin}/rbx" -e "puts RUBY_ENGINE"`.chomp
+  end
+end

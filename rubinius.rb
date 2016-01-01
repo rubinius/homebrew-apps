8575da801e6afbe4da03cc25e4aaf44de628bc05
Brian Shirai <brian@rubinius.com>
2016-01-01 09:48:10 -0800

    Version 2.71828182

--- rubinius.rb
+++ rubinius.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Rubinius < Formula
   homepage 'http://rubinius.com/'
   url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-2.71828182.tar.bz2'
-  sha1 'f22bba812c4a6eff404046f2dc3c3ccf57ab09b9'
+  sha1 '087044898dc3027b6a0caad5e49ad328ded6437e'
 
   depends_on 'libyaml'
 

943df4fe5b0ad2daef62ccaa628594d98f047567
Brian Shirai <brian@rubinius.com>
2016-06-12 09:35:07 -0700

    Version 3.40

--- rubinius.rb
+++ rubinius.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rubinius < Formula
   homepage 'http://rubinius.com/'
-  url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-3.39.tar.bz2'
-  sha256 '1858e9d120bd5f6a92b4d91a2fea1204b3111edded85c3ce23738bc789143d55'
+  url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-3.40.tar.bz2'
+  sha256 'b5bf9fd10e57bb2298355da8f19edb0f66b99536bbccf72d61a024982d376bba'
 
   depends_on 'libyaml'
 

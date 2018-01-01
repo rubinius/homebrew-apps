11206534a3c50a18baa71bbd3ffabd7792c3cc3e
Brian Shirai <brian@rubinius.com>
2017-12-31 17:49:56 -0800

    Version 3.87
    Revert to sha256

--- rubinius.rb
+++ rubinius.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Rubinius < Formula
   homepage 'http://rubinius.com/'
   url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-3.87.tar.bz2'
-  sha512 'c1abd03828b426782a09836cfefd63e04bbfd195128b86d2499cbc562a587d0cd732aa184d88031113a1da6f9d4d0be11a7207718af1ade1a00ff5c7ff34cd8d'
+  sha512 'f8fe5fa85daf8a7e01fffa012995fd635cf6d0e21e3002a3183a9a0f4a65d5f8'
 
   depends_on 'libyaml'
 

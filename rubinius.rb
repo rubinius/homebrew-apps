1bb7c35442b6cc8368d249cb60b1883db7769777
Brian Shirai <brian@rubinius.com>
2019-12-14 13:27:39 -0800

    Version 4.8

--- rubinius.rb
+++ rubinius.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rubinius < Formula
   homepage 'http://rubinius.com/'
-  url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-4.7.tar.bz2'
-  sha256 '9eda726490ceaa44fd24d9d1a7bc8dd4c8f511f04caf39403a9aeec75e9a5802'
+  url 'https://rubinius-binaries-rubinius-com.s3.amazonaws.com/homebrew/rubinius-4.8.tar.bz2'
+  sha256 ''
 
   depends_on 'libyaml'
 
